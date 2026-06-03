cadd8b0b29502f51845c69dcdcfc63fa65a6dcee
esnoko <elvisnoko18@gmail.com>
2026-06-03 09:07:35 +0200

    Enhance navbar with responsive menu toggle and updated styles

--- resources/views/workshops/navbar/index.blade.php
+++ resources/views/workshops/navbar/index.blade.php
@@ -6,7 +6,25 @@
     </div>
 
     <div class="topbar-nav-row">
-        <nav class="top-nav" aria-label="Primary">
+        <input id="page-nav-toggle" class="main-navigation-toggle" type="checkbox" aria-label="Toggle navigation" />
+        <label for="page-nav-toggle" class="menu-toggle" aria-controls="primary-navigation">
+            <svg class="icon--menu-toggle" viewBox="0 0 60 30" aria-hidden="true">
+                <g class="icon-group">
+                    <g class="icon--menu">
+                        <path d="M 6 0 L 54 0" />
+                        <path d="M 6 15 L 54 15" />
+                        <path d="M 6 30 L 54 30" />
+                    </g>
+                    <g class="icon--close">
+                        <path d="M 15 0 L 45 30" />
+                        <path d="M 15 30 L 45 0" />
+                    </g>
+                </g>
+            </svg>
+            <span class="menu-toggle-label">Menu</span>
+        </label>
+
+        <nav id="primary-navigation" class="top-nav" aria-label="Primary" data-nav-menu>
             <a href="{{ route('home') }}">Home</a>
             <a href="#about">About Us</a>
             <a href="{{ route('workshops.index') }}" class="active" aria-current="page">Workshops</a>
